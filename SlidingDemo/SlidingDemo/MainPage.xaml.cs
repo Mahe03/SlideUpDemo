7b81574646d39198bef1cfb92ce71c33d7c1ed20
Arumugam <P10462811@capita.co.uk>
2022-03-11 15:50:14 +0530

    Slide up demo

---
+++ SlidingDemo/SlidingDemo/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Xamarin.Forms;
+
+namespace SlidingDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        private bool slideUp;
+        private double initialBodyPosition;
+        async void Handele_Tapped(object sender, System.EventArgs e)
+        {
+            if(slideUp)
+            {
+                await body.TranslateTo(0, initialBodyPosition, 500, Easing.SinIn);
+
+                slideUp = false;
+            }
+            else
+            {
+                var initialPosition = TopLayout.Height;
+                var currentPosition = body.Height;
+
+                await body.TranslateTo(0, (initialPosition - currentPosition) * -1, 500, Easing.SinIn);
+
+                slideUp = true;
+            }
+            
+        }
+        public MainPage()
+        {
+            InitializeComponent();
+
+            initialBodyPosition = TopLayout.Height + 700;
+            body.TranslationY = initialBodyPosition; //App.Current.MainPage.Height - 100;
+        }
+    }
+}
